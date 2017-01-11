74b959c62abf7e17a671747192a82eb144e40c8e
alex <alexbonavila44@gmail.com>
2017-01-11 17:26:37 +0100

    delete unusable legacy code

--- ubuntu5.sh
+++
@@ -1,6 +0,0 @@
-wget download.netbeans.org/netbeans/8.1/final/bundles/netbeans-8.1-linux.sh
-sudo chmod 777 -R netbeans-8.1-linux.sh
-sudo ./netbeans-8.1-linux.sh
-sudo rm -rd netbeans-8.1-linux.sh
-
-
